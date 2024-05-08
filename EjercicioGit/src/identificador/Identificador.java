ae4a566e0590b82f54e2e5b5a2e7b7376d3a3e0c
alumnoT <alumnoT@INF2PC01>
2024-05-08 20:03:31 +0200

    Este mensaje es mi primer commit a un repositorio git en mi maquina local

---
+++ EjercicioGit/src/identificador/Identificador.java
@@ -0,0 +1,11 @@
+package identificador;
+
+public class Identificador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("XXXXXX");
+
+	}
+
+}
